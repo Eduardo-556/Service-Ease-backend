5a6937afef158407c4a805f95663da15a8f87a61
Eduardo <eduardo_silva800@hotmail.com>
2025-09-16 21:39:49 -0300

    feat: add bcrypt and refactor user to use hashed password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../database/prisma.service';
 import { PrismaError } from 'src/common/interfaces/prisma-error.interface';
+import password from './passwordService/password';
 
 @Injectable()
 export class UsersService {
@@ -10,6 +11,8 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto) {
     try {
+      const hashedPassword = await password.hash(createUserDto.password);
+      createUserDto.password = hashedPassword;
       await this.prismaService.user.create({
         data: { ...createUserDto },
       });
@@ -29,16 +32,41 @@ export class UsersService {
   }
 
   async findAll() {
-    const users = await this.prismaService.user.findMany();
+    const users = await this.prismaService.user.findMany({
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        phone: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
     return { users: users };
   }
 
   async findOne(id: string) {
-    const user = await this.prismaService.user.findUnique({ where: { id } });
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        phone: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    const userWithoutPassword = {
+      ...user,
+      password: undefined,
+    };
     if (!user) {
       throw new NotFoundException('User not found');
     }
-    return { user: user };
+    return { user: userWithoutPassword };
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
